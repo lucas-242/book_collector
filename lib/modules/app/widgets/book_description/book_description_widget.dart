525a8e3cdb46a441106cd3d7f88e593fb543c06f
Marcelo Antunes <mlsantunes@live.com>
2021-07-22 21:00:15 -0300

    beginning description page

--- lib/modules/app/widgets/book_description/book_description_widget.dart
+++ lib/modules/app/widgets/book_description/book_description_widget.dart
@@ -1,3 +1,4 @@
+import 'package:book_collector/shared/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDescriptionWidget extends StatelessWidget {
@@ -7,6 +8,24 @@ class BookDescriptionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Column(children: [Container(height: 300, color: Colors.blue)]);
+    return Container(
+      color: Colors.white,
+      width: size.width,
+      child: Column(children: [
+        Container(
+          alignment: Alignment.center,
+          height: size.height / 4,
+          color: Colors.blue,
+          child:
+              Text("foto do livro aqui", style: AppTextStyles.buttonBoldGrey),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+              style: AppTextStyles.buttonGrey),
+        )
+      ]),
+    );
   }
 }
